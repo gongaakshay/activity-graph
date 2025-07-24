fca343491e07ddd2b46eb4d78f09f252599e5424
gongaakshay0571@gmail.com <gongaakshay-571@gmail.com>
2025-07-25 03:23:07 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const jsonfile = require('jsonfile');
+const fs = require('fs');
 const moment = require('moment');
-const simpleGit = require('simple-git')
-const FILE_PATH = './data.json';
-const DATE = moment().format();
-const data ={
-    date : DATE
-}
-jsonfile.writeFile(FILE_PATH,data);
+const simpleGit = require('simple-git');
 
-simpleGit().add([FILE_PATH]).commit(DATE,{'~~date':DATE});
+const FILE_PATH = './data.json';
+const git = simpleGit();
+
+// Change the date here
+const DATE = moment().subtract(1, 'y').add(1, 'd').format('YYYY-MM-DDTHH:mm:ss');
+
+// Write something to file
+fs.writeFileSync(FILE_PATH, JSON.stringify({ date: DATE }));
+
+// Set env variables for fake commit date
+git.env({
+    GIT_AUTHOR_DATE: DATE,
+    GIT_COMMITTER_DATE: DATE,
+}).add([FILE_PATH])
+  .commit(`Backdated commit on ${DATE}`)
+  .push('origin', 'main')
+  .then(() => console.log('✅ Commit pushed with custom date!'))
+  .catch((err) => console.error('❌ Push failed:', err));
